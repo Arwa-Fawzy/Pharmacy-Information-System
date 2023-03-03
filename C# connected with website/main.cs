ff54b69a5806a2122ae2f864a1b7422990911a5f
Arwa Fawzy <101527083+Arwa-Fawzy@users.noreply.github.com>
2023-03-03 13:35:36 +0200

    Update main.cs

--- C# connected with website/main.cs
+++ C# connected with website/main.cs
@@ -5,7 +5,7 @@ namespace Namespace {
     public static class Module {
         
         public static object add() {
-            var con = sqlite3.connect(database: @"D:/CS/3rd/software-engineering/project/coding/Pharmacy_management-system-master/pharmacy.db");
+            var con = sqlite3.connect(database: @"./pharmacy.db");
             var cur = con.cursor();
             cur.execute("CREATE TABLE IF NOT EXISTS Information(REF_NO text PRIMARY KEY,COMPANY_NAME text,TYPE_OF_MED text,MED_NAME text,LOT_NO text,ISSUE_DT text,EXP_DT text,USES text,SIDE_EFFECT text,PRECAUTION text,DOSAGE text,PRICE text,QUANTITY text)");
             con.commit();
